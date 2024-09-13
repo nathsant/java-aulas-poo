187b549f211120034035f4a440efe271f3c6369c
nathsant <nath091santos@gmail.com>
2024-09-13 11:30:40 -0300

    associando valores a objetos

--- src/main/java/br/sesi/nss/java_aulas_poo/poo/Main.java
+++ src/main/java/br/sesi/nss/java_aulas_poo/poo/Main.java
@@ -1,9 +1,28 @@
 package br.sesi.nss.java_aulas_poo.poo;
 
+import br.sesi.nss.java_aulas_poo.poo.cabeca.Cabeca;
+import br.sesi.nss.java_aulas_poo.poo.cabeca.Olho;
+
 public class Main {
 
     public static void main(String[] args) {
+        Olho olho1 = new Olho();
+        olho1.cego = true;
+        olho1.corIris = "azul";
+        olho1.diametroOlho = 2.0f;
+
+        Olho olho2 = new Olho();
+        olho2.cego = false;
+        olho2.corIris = "castanho";
+        olho2.diametroOlho = 2.5f;
+
+        Cabeca cabeca = new Cabeca();
+        cabeca.olhoEsquerdo = olho1;
+        cabeca.olhoDireito = olho2;
+
         pessoa pessoa1 = new pessoa();
-        pessoa1.cabeca.olho.corIris = "marrom";
+
+        pessoa1.cabeca = cabeca;
+
     }
 }
--- src/main/java/br/sesi/nss/java_aulas_poo/poo/cabeca/Cabeca.java
+++ src/main/java/br/sesi/nss/java_aulas_poo/poo/cabeca/Cabeca.java
@@ -3,11 +3,13 @@ package br.sesi.nss.java_aulas_poo.poo.cabeca;
 public class Cabeca {
     public Orelha orelha;
     public Nariz nariz;
-    public Olho olho;
+    public Olho olhoEsquerdo;
+    public Olho olhoDireito;
     public Boca boca;
 
-    public Cabeca(){
-        this.olho = new Olho();
+    public Cabeca() {
+        this.olhoEsquerdo = new Olho();
+        this.olhoDireito = new Olho();
         this.orelha = new Orelha();
         this.nariz = new Nariz();
         this.boca = new Boca();
